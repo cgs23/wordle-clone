59e97c39aadde7bb14c67a730f3ae010008434a2
Stavros Gian <stavrgiannakis@gmail.com>
2022-02-20 20:50:33 +0100

    same word for everyone per day

--- src/models/Grid.ts
+++ src/models/Grid.ts
@@ -2,6 +2,7 @@ import { GameStatus } from "../constants/enums";
 import { GridRowModel } from "./GridRow";
 import { IGridModel } from "./IModels";
 import answers from "../constants/answers";
+import seedrandom from 'seedrandom';
 
 export class GridModel implements IGridModel {
   rows!: GridRowModel[];
@@ -12,9 +13,12 @@ export class GridModel implements IGridModel {
   constructor() {
     this.currentRow = 0;
     this.currentTile = 0;
+    let rng = seedrandom(new Date().toDateString());
     this.gameStatus = GameStatus.PLAYING;
     this.rows = [];
-    const idx = Math.floor(Math.random() * answers.length);
+    console.log(Math.floor(rng() * answers.length));
+    
+    const idx = Math.floor(rng() * answers.length);
     this.hiddenWord = answers[idx];
     for (let index = 0; index < 6; index++) {
       this.rows.push(new GridRowModel());
